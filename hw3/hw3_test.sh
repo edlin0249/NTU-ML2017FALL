627ebffdf85a2698199219c6ade534e321b47e32
edlin0249 <b03902125@ntu.edu.tw>
2017-11-20 00:34:06 +0800

    v6

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-wget 'https://www.dropbox.com/s/wa01j98nfyg6j22/model-600.h5?dl=1'
+wget 'https://github.com/edlin0249/ML2017FALL/releases/download/model/model-600.h5'
 python3 hw3_test.py $1 $2
